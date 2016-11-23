98b2f0ed5fc6a2d8961977c4dc7d99219ab41668
下舘朗 <shimodateakira@i.softbank.jp>
2016-11-23 21:42:08 +0900

    初期コミット

--- ConsoleTestTuple/Program.cs
+++ ConsoleTestTuple/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,27 @@ namespace ConsoleTestTuple
     {
         static void Main(string[] args)
         {
+            var a = (1, "AKIRA");
+            Trace.WriteLine($"Item1={a.Item1}, Item2={a.Item2}");
+
+            var happyBirthDay = HappyBirthDay(new DateTime(1971, 10, 12), "AKIRA");
+            Trace.WriteLine($"{happyBirthDay.Age}: {happyBirthDay.Message}");
+        }
+
+        static (int Age, string Message) HappyBirthDay(DateTime birth, string name)
+        {
+            return ((DateTime.Now - birth).GetYears(), $"Happy birth day! {name}!");
+        }
+    }
+    public static class TimeSpanExtensions
+    {
+        public static int GetYears(this TimeSpan timespan)
+        {
+            return (int)((double)timespan.Days / 365.2425);
+        }
+        public static int GetMonths(this TimeSpan timespan)
+        {
+            return (int)((double)timespan.Days / 30.436875);
         }
     }
 }
